1a2187fe7ab635f326ac98a32bf0fac8e31c0da0
jrizz <juan.rizzato@uner.edu.ar>
2025-01-26 20:25:23 -0300

    actualizacion

--- Clasificador de spam/main.py
+++ Clasificador de spam/main.py
@@ -9,6 +9,8 @@ from sklearn import preprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer
 import tensorflow as tf
 from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 
@@ -95,9 +97,66 @@ model.add(keras.layers.Dense(N_HIDDEN,
 
 model.add(keras.layers.Dense(NB_CLASSES,
                             name='Output-Layer',
-                            activation='softmax'))
+                            activation='softmax')) # softmax es una función de activación 
+                                                   # que se utiliza en la capa de salida de una red neuronal 
+                                                   # para convertir los valores de la capa de salida en probabilidades
 
 model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 model.summary()
+
+
+# el modelo ya fue creado, procedemos al antenamiento del modelo
+# seteamos verbose a 1 para que nos muestre el progreso del entrenamiento
+VERBOSE = 1
+# seteamos los hyperparámetros de entrenamiento
+EPOCHS = 10
+BATCH_SIZE = 256
+VALIDATION_SPLIT = 0.2
+
+print('Entrenando el modelo...\n------------------------------------')
+history = model.fit(x_train, y_train,
+                    batch_size=BATCH_SIZE,
+                    epochs=EPOCHS,
+                    verbose=VERBOSE,
+                    validation_split=VALIDATION_SPLIT)
+# history es un objeto que contiene la información del entrenamiento del modelo
+# contiene los parametros de entrenamiento y validación, como la precisión y la pérdida
+
+
+# evaluamos el modelo
+print('\nPrecision durante el entrenamiento\n------------------------------------')
+pd.DataFrame(history.history).plot(figsize=(8, 5))  
+# asi se prodrian mostrar las graficas por separado, cada una con un show() correspondiente
+# pd.DataFrame(history.history)["accuracy"].plot(figsize=(8, 5))
+# pd.DataFrame(history.history)["loss"].plot(figsize=(8, 5))
+# pd.DataFrame(history.history)["val_accuracy"].plot(figsize=(8, 5))
+# pd.DataFrame(history.history)["val_loss"].plot(figsize=(8, 5))
+plt.title('Precision durante el entrenamiento')
+plt.show()
+
+print('\nEvaluando el modelo...\n------------------------------------')
+model.evaluate(x_test, y_test, verbose=VERBOSE)
+
+
+# predicciones del modelo
+
+# Predecir para múltiples muestras utilizando procesamiento por lotes
+
+predict_tfidf = vectorizer.transform(['FREE entry to a fun contest',
+                                      'Hey, how are you doing?',
+                                      'WIN a super prize today',
+                                      'do you want a loan?']).toarray()
+
+print('\nPredicciones del modelo\n------------------------------------')
+print(predict_tfidf.shape) # el metodo shape nos muestra la forma de la matriz
+                           # que puede ser entendida como el número de filas y columnas de la matriz
+
+# preccion usando el modelo
+predictions = model.predict(predict_tfidf)
+prediction = np.argmax(predictions, axis=1)
+print('Prediccon: ', prediction)
+
+# las clases de predicciones
+print('Clases de predicciones: ', le.inverse_transform(prediction))
