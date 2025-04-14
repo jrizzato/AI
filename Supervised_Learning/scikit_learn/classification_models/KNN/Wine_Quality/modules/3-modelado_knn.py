d67b49cbbf475e4f87b8e28f9dc6a7e0453d30a9
jrizzato <juan.rizzato@uner.edu.ar>
2025-04-14 19:58:44 -0300

    agregado stars classification

--- Supervised_Learning/scikit_learn/classification_models/KNN/Wine_Quality/modules/3-modelado_knn.py
+++ Supervised_Learning/scikit_learn/classification_models/KNN/Wine_Quality/modules/3-modelado_knn.py
@@ -16,13 +16,23 @@ from sklearn.metrics import accuracy_score
 
 training_accuracy = []
 test_accuracy = []
-k_values = range(1, 30)  # Probar valores de k de 1 a 20
+k_values = range(1, 30)  # Probar valores de k de 1 a 29, los indices van a ser 0 a 28
+k_val_max = 0
+knn_score_max = 0
 for k in k_values:
     knn = KNeighborsClassifier(n_neighbors=k)
     knn.fit(X_train, y_train)
     training_accuracy.append(knn.score(X_train, y_train))
-    test_accuracy.append(knn.score(X_test, y_test))
+    knn_score = knn.score(X_test, y_test)
+    if knn_score > knn_score_max and k > 3:
+        knn_score_max = knn_score
+        k_val_max = k
+        print(f"Mejor k: {k_val_max} con precisión: {knn_score_max:.2f}")
+    test_accuracy.append(knn_score)
 
+knn = KNeighborsClassifier(n_neighbors=k_val_max)
+knn.fit(X_train, y_train)
+    
 # graficamos la precisión del modelo
 import matplotlib.pyplot as plt
 plt.figure(figsize=(10, 6))
@@ -51,7 +61,27 @@ print(f"Predicción de calidad para el vino de prueba: {y_vino[0]}")
 # Hacer predicciones
 y_pred = knn.predict(X_test)
 
-
 # Evaluar el modelo
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Precisión del modelo KNN: {accuracy:.2f}")
+
+print("------------------------------------------------------")
+# # Seleccionar dos características (por ejemplo, 'alcohol' y 'pH')
+# X_train_2d = X_train[:, [9, 8]]  # Índices de las columnas seleccionadas
+# X_test_2d = X_test[:, [9, 8]]
+
+# # Entrenar el modelo KNN con las dos características seleccionadas
+# knn_2d = KNeighborsClassifier(n_neighbors=k_val_max)
+# knn_2d.fit(X_train_2d, y_train)
+
+# # Graficar las fronteras de decisión
+# import mglearn
+# plt.figure(figsize=(10, 6))
+# mglearn.plots.plot_2d_separator(knn_2d, X_train_2d, fill=True, alpha=0.3)
+# mglearn.discrete_scatter(X_train_2d[:, 0], X_train_2d[:, 1], y_train)
+# plt.title(f"Fronteras de decisión del modelo KNN (k={k_val_max})")
+# plt.xlabel("Alcohol")
+# plt.ylabel("pH")
+# plt.legend(["Clase 0", "Clase 1", "Clase 2"], loc="best")
+# plt.grid()
+# plt.show()
